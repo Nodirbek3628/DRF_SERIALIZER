309d629d9bbb420576e043ccacf7062d960c3ade
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-12-04 22:19:30 +0500

    urls

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=128,unique=True)
+    description = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    name = models.CharField(max_length=128,unique=True)
+    description = models.TextField(blank=True,null=True)
+    status = models.BooleanField(default=False)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,)
+
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    def __repr__(self):
+        return self.name
+
+
+    
